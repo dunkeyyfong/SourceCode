35f7a2aa7c23af1bda7c5f7516a987f982c529e3
Fong <126961584+dunkeyyfong@users.noreply.github.com>
2023-09-10 16:15:17 +0700

    Initial Commit

---
+++ Wibu Chat/Wibu_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wibu_ChatApp.swift
+//  Wibu Chat
+//
+//  Created by DunkeyyFong on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Wibu_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
